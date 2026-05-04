ccfbf8f0cd7f2e6e585eb01a73ed4310f1c8321b
albinlon <albinlon@chalmers.se>
2026-05-04 15:32:51 +0200

    färg

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -16,23 +16,46 @@ class MainView extends StatelessWidget {
     // Den kan vara enklare att förstå.
     // Denna version har fördelen att kort skapas on-demand.
     return Scaffold(
-      appBar: AppBar(title: const Text('iMats produkter')),
-      body: Padding(
-        padding: const EdgeInsets.all(AppTheme.paddingSmall),
-        child: GridView.builder(
-          itemCount: products.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4, // 4 kolumner
-            crossAxisSpacing: AppTheme.paddingSmall,
-            mainAxisSpacing: AppTheme.paddingSmall,
-            childAspectRatio: 4 / 3,
-          ),
-          itemBuilder: (context, index) {
-            final product = products[index];
+      appBar: AppBar(
+        title: const Text('iMats produkter'),
+        backgroundColor: const Color(0xFF34B5F0),
+        foregroundColor: Colors.white,
+      ),
 
-            return ProductCard(product, iMat);
-          },
-        ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(12),
+            color: const Color(0xFF81D7FF),
+            child: const Text(
+              'Alla produkter',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(AppTheme.paddingSmall),
+              child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: AppTheme.paddingSmall,
+                  mainAxisSpacing: AppTheme.paddingSmall,
+                  childAspectRatio: 4 / 3,
+                ),
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return ProductCard(product, iMat);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
