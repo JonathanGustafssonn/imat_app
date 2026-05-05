210f8e7a257722c04fd4d15e6c5e79a1e0791067
Zétény Mátó <zeti.mato@gmail.com>
2026-05-05 16:50:09 +0200

    Har lagt till produktrutan till startsidan

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imat_app/app_theme.dart';
 import 'package:imat_app/model/imat_data_handler.dart';
+import 'package:imat_app/widgets/filter_button.dart';
 import 'package:imat_app/widgets/product_card.dart';
 import 'package:provider/provider.dart';
 import 'package:imat_app/pages/profile.dart';
@@ -104,24 +105,61 @@ class MainView extends StatelessWidget {
                 BuildIconButton(Icons.store),
                 BuildIconButton(Icons.favorite),
               ],
-              ),
             ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 150, vertical: 10),
+            child: Row(
+              children: [
+                FilterButton(
+                  icon: Icons.filter_list,
+                  label: 'Filtrera',
+                  iconColor: Colors.white,
+                  backgroundColor: const Color(0xFF34B5F0),
+                  onTap: () {},
+                ),
+                const SizedBox(width: 15),
+                FilterButton(
+                  icon: Icons.star,
+                  label: 'Rea',
+                  iconColor: Colors.yellow,
+                  backgroundColor: const Color(0xFF34B5F0),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
 
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(AppTheme.paddingSmall),
-              child: GridView.builder(
-                itemCount: products.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 5,
-                  crossAxisSpacing: AppTheme.paddingSmall,
-                  mainAxisSpacing: AppTheme.paddingSmall,
-                  childAspectRatio: 3 / 4,
+              padding: const EdgeInsets.symmetric(horizontal: 150),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFEFEFEF),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(0),
+                    bottomRight: Radius.circular(0),
+                  ),
+                  border: Border.all(color: Colors.black),
                 ),
-                itemBuilder: (context, index) {
-                  final product = products[index];
-                  return ProductCard(product, iMat);
-                },
+
+                padding: const EdgeInsets.all(AppTheme.paddingSmall),
+                child: GridView.builder(
+                  itemCount: products.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 5,
+                    crossAxisSpacing: AppTheme.paddingSmall,
+                    mainAxisSpacing: AppTheme.paddingSmall,
+                    childAspectRatio: 3 / 4,
+                  ),
+                  itemBuilder: (context, index) {
+                    final product = products[index];
+                    return ProductCard(product, iMat);
+                  },
+                ),
               ),
             ),
           ),
